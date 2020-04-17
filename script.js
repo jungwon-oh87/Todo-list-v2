00e3450bfab35c336f5a5e9818f76f588db93bd2
Jungwon Oh <jungwon.oh87@gmail.com>
2020-04-17 16:22:05 -0700

    list filter completed

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const input = document.getElementById("input");
 const add_btn = document.getElementById("add-btn");
 const lists = document.getElementById("lists");
+const select_el = document.querySelector(".filter-todo");
 
 function addDOM(e) {
   e.preventDefault();
   const input_value = input.value;
+  console.log(input_value);
+  if (input_value === "") {
+    alert("input required..");
+    return;
+  }
 
   const div_el = document.createElement("div");
   div_el.classList.add("list_div");
@@ -43,6 +49,38 @@ function deleteList(e) {
   }
 }
 
+function filterResult(e) {
+  const category = e.target.value;
+  const ul_children = lists.childNodes;
+
+  ul_children.forEach((l) => {
+    console.log(category);
+    console.log(l.classList);
+    // completed only
+    if (category === "completed" && l.classList.contains("completed")) {
+      l.style.display = "flex";
+    } else if (category === "completed" && !l.classList.contains("completed")) {
+      l.style.display = "none";
+    }
+
+    // incompleted only
+    else if (category === "incompleted" && !l.classList.contains("completed")) {
+      console.log("incompleted called");
+      l.style.display = "flex";
+    } else if (
+      category === "incompleted" &&
+      !l.classList.contains("incompleted")
+    ) {
+      l.style.display = "none";
+    }
+
+    // all
+    else if (category === "all") {
+      l.style.display = "flex";
+    }
+  });
+}
+
 function init() {
   input.value = "";
   input.focus();
@@ -51,3 +89,4 @@ function init() {
 init();
 add_btn.addEventListener("click", addDOM);
 lists.addEventListener("click", deleteList);
+select_el.addEventListener("change", filterResult);
